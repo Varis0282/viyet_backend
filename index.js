7868bd3435caa00aa012485e98013e816f137ace
Varis0282 <varisrajakforth11@gmail.com>
2024-04-03 16:04:25 +0530

    Refactor server startup and database connection

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
-require('./dbconfig');
+const mongoose = require('mongoose');
 require('dotenv').config();
+const Mongo_URI = process.env.MONGO_URI;
 
 const movieRoutes = require('./routes/movieRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -33,6 +33,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+mongoose.connect(Mongo_URI)
+    .then(() => {
+        console.log('Connected to the database');
+        try {
+            app.listen(port, () => {
+                console.log(`Server is running on ${port}`);
+            });
+        } catch (err) {
+            console.log('Error starting the server', err);
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        console.log('Error connecting to the database');
+    });
